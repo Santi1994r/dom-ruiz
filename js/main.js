75c2e482b201295f548a01b047a384b547f42eef
santiago ruiz <santiagoruiz9416@gmail.com>
2022-06-24 18:21:57 -0300

    agrego otros archivos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-let primerTitulo = ()=>{
+/* let primerTitulo = ()=>{
     alert("Urbanos/ Ropa para Hombres")
 }
 
 let tituloPrinc = document.getElementById("tituloPrincipal");
-
-/* tituloPrinc.onclick = primerTitulo; */
-tituloPrinc.addEventListener("click", primerTitulo);
+ */
+/* tituloPrinc.onclick = primerTitulo; 
+tituloPrinc.addEventListener("click", primerTitulo);*/
 /*
 El metodo mas recomendable es este addEventListener que como primer parametro va el evento que nosotros requerimos y como segundo parametro va una funcion o mejor echo seria que halla el nombre de la variable o constante que contiene una funcion.
 */
+
+/*
+Voy a intentar crear un evento que al tocar ver productos me muestres mis productos, si es que se puede.Por lo menos que me muestre lo que contiene cada objeto
+ */
+
+//variable que contiene el boton de ver productos en el index
+let verProductos = document.getElementById("verProduc");
+
+//Funcion para que me mapee y me muestre todos los productos dentro de mi array
+let verTodosLosProductos = () => {
+
+}
+
+verProductos.addEventListener("click", )
